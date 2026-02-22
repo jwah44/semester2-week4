fb705c3b374876247c5cde37c6a51aa12b466fa9
jwah44 <zxck0708@leeds.ac.uk>
2026-02-22 20:19:03 +0000

    w4s2 complete ish

--- session-2/task2/string_ops.c
+++ session-2/task2/string_ops.c
@@ -12,12 +12,16 @@ int main( void ) {
     char str3[100];
 
     // use printf to verify the following tests
-
+    printf("%s\n%s\n", str1, str2);
     // use 'strcmp' to compare 2 strings - print the return value
-
+    int comparison;
+    comparison = strcmp(str1, str2);
+    printf("%d\n", comparison);
     // use 'strcat' to concatenate 2 strings - print the resulting string
-
+    strcat(str1, str2);
+    printf("%s\n", str1);
     // use 'strcpy' to copy a string into str3 - print string 3
-
+    strcpy(str3, str1);
+    printf("%s", str3);
     return 0;
 }
--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 
 int main( void ) {
+    char buffer[10];
+    int x, y;
+
+    printf("Enter two integers separated by a space:");
+    fgets(buffer, sizeof(buffer), stdin);
+
+    if(sscanf(buffer, "%d %d", &x, &y) == 2) {
+        int sum = x + y;
+        printf("The sum of %d and %d is %d", x, y, sum);
+    } else {
+    (printf("Invalid input"));
+    }
 
     // define suitable data
 
